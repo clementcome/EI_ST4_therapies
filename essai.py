6ce71ce0720dc88b0a84fba144bf25be9b41c49d
Baril <anne.baril@student-cs.fr>
2019-06-17 16:15:59 +0200

    Bandwith/frequence

--- essai.py
+++ essai.py
@@ -1,3 +1,7 @@
-Test
+import numpy as np, pandas as pd; np.random.seed(0)
+import seaborn as sns
+sns.set(style="white", color_codes=True)
+g = sns.jointplot(x=[1,2,3], y=[4,5,6])
+
 
 
---
+++ tinnituse_feature.py
@@ -0,0 +1,40 @@
+import json
+import datetime
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+def tinnituses_features(data):
+    with open(data) as json_es:
+        abon = json.load(json_es)
+    tinnituse = []
+    count_error = 0
+    for user in abon :
+        try :
+            frequence = abon[user]["reducers"]["tinnituses"][0]["frequency"]
+            bandwidth = abon[user]["reducers"]["tinnituses"][0]["bandwidth"]
+            tinnituse.append((frequence, bandwidth))
+        except :
+            count_error += 1
+    return(tinnituse,count_error)
+
+
+tinnituse = tinnituses_features('data/dsu.json')[0]
+
+X = [val[0] for val in tinnituse]
+Y = [val[1] for val in tinnituse]
+plt.xlabel("Frequences")
+plt.ylabel("Bandwith")
+plt.scatter(X,Y, color='fuchsia')
+plt.show()
+
+
+
+
+
+
+
+
+
+
