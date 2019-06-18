80e9898837f707d2cbdd818739bbc9071e266cdc
mculot <maxime.culot@student-cs.fr>
2019-06-18 18:05:57 +0200

    Fréquence d'utilisation des utilisateurs en fonction de la fréquence de l'acouphène

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from project.explore import list_activities, get_1_acouphenometry, get_trajector
 from project.visu import display_1_acouphenometry, display_trajectory
 from project.extract import extract_activity_data
 from project.activity_analysis import therapy_analysis
-
+from project.scores_successifs import score
 # d_activities, count_error = list_activities()
 from project.explore import list_activities, get_1_acouphenometry, list_activities_one_user,freq_keepTheShape, plot_two_activities, plot_all_activities, plot_diag
 d_activities, count_error = list_activities()
@@ -36,18 +36,25 @@ plt.yscale('log')
 print(plot_two_activities()[0],plot_two_activities()[1])
 plt.show()'''
 
+#
+# matrixplot=sns.pairplot(plot_all_activities())
+# #diagplot=sns.pairplot(plot_diag())
+#
+#
+# plt.plot([0,200],[0,200], color="red")
+# for i in range(0, len(matrixplot.axes)):
+#             for j in range(0, len(matrixplot.axes)):
+#                 matrixplot.axes[i,j].set(xscale='log', yscale='log')   #, xlim=(1,200), ylim=(1,200))
+#
+# #plt.savefig("/Users/maximeculot/Downloads/figure.png",dpi=500)
+# plt.savefig("/Users/maximeculot/Downloads/figure.png")
 
-matrixplot=sns.pairplot(plot_all_activities())
+
 #diagplot=sns.pairplot(plot_diag())
-
-
-plt.plot([0,200],[0,200], color="red")
-for i in range(0, len(matrixplot.axes)):
-            for j in range(0, len(matrixplot.axes)):
-                matrixplot.axes[i,j].set(xscale='log', yscale='log')   #, xlim=(1,200), ylim=(1,200))
-
-#plt.savefig("/Users/maximeculot/Downloads/figure.png",dpi=500)
-plt.savefig("/Users/maximeculot/Downloads/figure.png")
+# for i in range(0,len(diagplot.axes)):
+#     for j in range(0,len(diagplot.axes)):
+#         diagplot.axes[i,j].set
+#plt.show()
 
 # data_acouphenometry = get_1_acouphenometry()
 # print(data_acouphenometry["data"].keys())
@@ -57,12 +64,8 @@ plt.savefig("/Users/maximeculot/Downloads/figure.png")
 # get_trajectories_acouphenometry()
 
 # display_trajectory(200)
-# extract_activity_data()
+extract_activity_data()
 
-therapy_analysis()
+#therapy_analysis()
 
-#diagplot=sns.pairplot(plot_diag())
-# for i in range(0,len(diagplot.axes)):
-#     for j in range(0,len(diagplot.axes)):
-#         diagplot.axes[i,j].set
-plt.show()
+print (score())
--- project/scores_successifs.py
+++ project/scores_successifs.py
@@ -1,1 +1,26 @@
+import json
+import matplotlib.pyplot as plt
+import pandas
+import numpy as np
+
+
 #scores successifs pour une activité [ des[data][gameScore] + des[userKey] where des[type] = “ACTIVITY_COMPLETE” ]
+def score(file="data/data_activity.json"):
+    users={}
+    with open(file) as input_file:
+        data = json.load(input_file)["es"]
+    #print(data)
+    for des in data.keys():
+        print(des)
+        if data[des]["type"]=='ACTIVITY_COMPLETE':
+            if data[des]["userKey"] not in users.keys():
+                # print(data[des])
+                # print(data[des]["userKey"])
+                # print(users)
+                users[data[des]["userKey"]]=[]  #Liste qui contiendra les score et leur date
+                users[data[des]["userKey"]].append([data[des]["data"]["gameScore"],(data[des]["date"])])
+            else:
+                users[data[des]["userKey"]].append([data[des]["data"]["gameScore"],(data[des]["date"])])
+    return users
+
+print (score())
